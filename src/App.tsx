66d02a6376a890ffc8be9fd4a0b95354f38a98ea
Alkonosttt <voodoomon@protonmail.com>
2025-05-19 21:25:07 +0200

    Added the styling for the light and dark theme, added a button to toggle between them

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-//import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
+  const [theme, setTheme] = useState<'dark'| 'light'>('light');
+
+  useEffect(()=> {
+    document.documentElement.classList.remove('dark', 'light');
+    document.documentElement.classList.add(theme);
+  },[theme]);
   
   return (
     <>
+      <button onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}/>
       <p>Portfolio Website</p>
     </>
   )
