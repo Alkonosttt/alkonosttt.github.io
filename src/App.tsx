d187f470ab2225851477dbc7dd794ddd02b233e9
Alkonosttt <voodoomon@protonmail.com>
2025-05-20 11:53:16 +0200

    Removed the unneeded aliases from the App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router} from 'react-router-dom'
 import TopBar from './components/TopBar'
 
 function App() {
