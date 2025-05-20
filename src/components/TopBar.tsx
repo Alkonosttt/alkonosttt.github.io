253f2bfeaffb333266a9a775170bd6319c3ef611
Alkonosttt <voodoomon@protonmail.com>
2025-05-20 14:29:03 +0200

    Added the button that links to GitHub

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -16,14 +16,22 @@ const TopBar: React.FC<TopBarProps> = () => {
         document.documentElement.classList.add(theme);
     },[theme]);
 
+    // to open my github using a button instead of a link
+    const githubLink = () => {
+        window.open('https://github.com/Alkonosttt', '_blank', 'noopener, noreferrer');
+    };
+
     return (
-        //clock
-        // a button to switch between the light and dark mode
+        // clock <Clock/>
+        // home button #home
+        // a button to switch between the light and dark mode #themeSwitcher
+        // a button with a link to GitHub #githubLink
         <div className="headerContainer">            
             <header className="TopBar">
                 <Clock/>
-                <button id="themeSwitcher" onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}/>
                 <button id="home" />
+                <button id="themeSwitcher" onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}/>
+                <button id="githubLink" onClick={githubLink}/>                
             </header>
         </div>
     );
